97bfb668b936ebc5cbf9628fd9ffa1448b79957f
Yash Patni <121944122+yash-patni@users.noreply.github.com>
2025-02-17 15:06:33 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/k largest elements/k-largest-elements.cpp
@@ -0,0 +1,61 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+class Solution {
+  public:
+    vector<int> kLargest(vector<int>& arr, int k) {
+        // Your code here
+        priority_queue<int, vector<int>, greater<int>> pq(arr.begin(), arr.begin()+k);
+        
+        for (int i=k; i<arr.size(); i++) {
+            if (pq.top() < arr[i]) {
+                pq.pop();
+                pq.push(arr[i]);
+            }
+        }
+        
+        vector<int> ans(k);
+        int i=k-1;
+        
+        while (!pq.empty()) {
+            ans[i] = pq.top();
+            pq.pop();
+            i--;
+        }
+        
+        return ans;
+    }
+};
+
+//{ Driver Code Starts.
+int main() {
+    string ts;
+    getline(cin, ts);
+    int t = stoi(ts);
+    while (t--) {
+
+        vector<int> arr;
+        string input;
+        getline(cin, input);
+        stringstream ss(input);
+        int number;
+        while (ss >> number) {
+            arr.push_back(number);
+        }
+        string ks;
+        getline(cin, ks);
+        int k = stoi(ks);
+        Solution ob;
+        vector<int> ans = ob.kLargest(arr, k);
+        for (auto it : ans) {
+            cout << it << " ";
+        }
+        cout << endl;
+        cout << "~" << endl;
+    }
+    return 0;
+}
+// } Driver Code Ends
