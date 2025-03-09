82baa0a8252b09c20249e5dd9a8859935af09319
Yash Patni <121944122+yash-patni@users.noreply.github.com>
2025-03-10 01:38:43 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Hard/Edit Distance/edit-distance.py
@@ -0,0 +1,31 @@
+class Solution:
+	def editDistance(self, s1, s2):
+		# Code here
+        m, n = len(s1), len(s2)
+        dp = [[0]*(n+1) for _ in range(m+1)]
+        for i in range(m+1):
+            dp[i][0] = i
+        for j in range(n+1):
+            dp[0][j] = j
+            
+        for i in range(m):
+            for j in range(n):
+                if s1[i] == s2[j]:
+                    dp[i+1][j+1] = dp[i][j]
+                else:
+                    dp[i+1][j+1] = min(dp[i+1][j], dp[i][j+1], dp[i][j]) + 1
+        return dp[-1][-1]
+
+#{ 
+ # Driver Code Starts
+if __name__ == '__main__':
+    T = int(input())
+    for i in range(T):
+        s1 = input()
+        s2 = input()
+        ob = Solution()
+        ans = ob.editDistance(s1, s2)
+        print(ans)
+        print("~")
+
+# } Driver Code Ends
