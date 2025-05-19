11950ecc5c1734e397d3130fe942dc31b9e733c7
Yash Patni <121944122+yash-patni@users.noreply.github.com>
2025-05-19 20:39:41 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Predecessor and Successor/predecessor-and-successor.cpp
@@ -0,0 +1,166 @@
+//{ Driver Code Starts
+// C++ program to find predecessor and successor in a BST
+#include "bits/stdc++.h"
+using namespace std;
+
+// BST Node
+class Node {
+  public:
+    int data;
+    Node* left;
+    Node* right;
+
+    Node(int x) {
+        data = x;
+        left = NULL;
+        right = NULL;
+    }
+};
+
+
+// } Driver Code Ends
+
+/* BST Node
+class Node
+{
+   public:
+    int data;
+    Node *left;
+    Node *right;
+
+    Node(int x){
+        data = x;
+        left = NULL;
+        right = NULL;
+    }
+}; */
+
+class Solution {
+  public:
+    void pre(Node* root , int&key , vector<Node*>&ans){
+        if(root == NULL) return ;
+        if(root -> data < key){
+            if(ans[0] -> data < root -> data) ans[0] = root ; ;
+        }
+        if(root -> data >= key) pre(root -> left , key , ans) ;
+        else pre(root -> right , key , ans) ;
+    }
+    
+    void succ(Node* root , int&key , vector<Node*>&ans){
+        if(root == NULL) return ;
+        if(root -> data > key){
+            if(ans[1] -> data > root -> data) ans[1] = root ; ;
+        }
+        if(root -> data > key) succ(root -> left , key , ans) ;
+        else succ(root -> right , key , ans) ;
+    }
+    
+    vector<Node*> findPreSuc(Node* root, int key) {
+        Node*temp = new Node(-1) ;
+        vector<Node*>ans = {new Node(-1) , new Node(INT_MAX)} ;
+        bool res = 0 ;
+        pre(root , key , ans) ;
+        succ(root , key , ans) ;
+        if(ans[1] -> data == INT_MAX) ans[1] = new Node(-1) ;
+        return ans ;
+    }
+};
+
+
+//{ Driver Code Starts.
+
+Node* buildTree(string str) {
+    // Corner Case
+    if (str.length() == 0 || str[0] == 'N')
+        return NULL;
+
+    // Creating vector of strings from input
+    // string after spliting by space
+    vector<string> ip;
+
+    istringstream iss(str);
+    for (string str; iss >> str;)
+        ip.push_back(str);
+
+    // Create the root of the tree
+    Node* root = new Node(stoi(ip[0]));
+
+    // Push the root to the queue
+    queue<Node*> queue;
+    queue.push(root);
+
+    // Starting from the second element
+    int i = 1;
+    while (!queue.empty() && i < ip.size()) {
+
+        // Get and remove the front of the queue
+        Node* currNode = queue.front();
+        queue.pop();
+
+        // Get the current node's value from the string
+        string currVal = ip[i];
+
+        // If the left child is not null
+        if (currVal != "N") {
+
+            // Create the left child for the current node
+            currNode->left = new Node(stoi(currVal));
+
+            // Push it to the queue
+            queue.push(currNode->left);
+        }
+
+        // For the right child
+        i++;
+        if (i >= ip.size())
+            break;
+        currVal = ip[i];
+
+        // If the right child is not null
+        if (currVal != "N") {
+
+            // Create the right child for the current node
+            currNode->right = new Node(stoi(currVal));
+
+            // Push it to the queue
+            queue.push(currNode->right);
+        }
+        i++;
+    }
+
+    return root;
+}
+
+// Driver program to test above functions
+int main() {
+
+    int t;
+    string tc;
+    getline(cin, tc);
+    t = stoi(tc);
+    while (t--) {
+        string s;
+        getline(cin, s);
+        Node* root = buildTree(s);
+        getline(cin, s);
+        int key = stoi(s);
+
+        Solution ob;
+        vector<Node*> result = ob.findPreSuc(root, key);
+        Node* pre = result[0];
+        Node* suc = result[1];
+
+        if (pre != nullptr)
+            cout << pre->data << " ";
+        else
+            cout << -1 << " ";
+
+        if (suc != nullptr)
+            cout << suc->data << endl;
+        else
+            cout << -1 << endl;
+        cout << "~" << endl;
+    }
+    return 0;
+}
+// } Driver Code Ends
