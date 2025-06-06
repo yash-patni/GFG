7bc0b8ee065e5a7192b63c1e5ddeb8655ca2e3f4
Yash Patni <121944122+yash-patni@users.noreply.github.com>
2025-06-06 08:10:11 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Search Pattern (Rabin-Karp Algorithm)/search-pattern-rabin-karp-algorithm.cpp
@@ -0,0 +1,36 @@
+class Solution {
+  public:
+    vector<int> search(string &pat, string &txt) {
+        // code here.
+        int mod = 1e5;
+        vector<int>ans;
+            int txtHash=0,patHash=0;
+            int n=txt.size();
+            int m=pat.size();
+            
+            for(int i=0;i<m;i++)
+            {
+                txtHash = ((txt[i]-'a')+txtHash)%mod;
+                patHash = ((pat[i]-'a')+patHash)%mod;
+            }
+            
+            if(txtHash==patHash)
+            {
+                if(txt.substr(0,m)==pat)
+                ans.push_back(1);
+            }
+            
+            for(int i=m,j=0;i<txt.size();i++,j++)
+            {
+               txtHash = (txtHash-txt[j])%mod;
+               txtHash = (txtHash+txt[i])%mod;
+               
+               if(txtHash==patHash)
+               {
+                  if(txt.substr(j+1,m)==pat)
+                  ans.push_back(j+2);
+               }
+            }
+        return ans;
+    }
+};
